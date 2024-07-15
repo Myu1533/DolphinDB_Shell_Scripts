00b6ff24c20cda492106cecd57bd2885c4711f9a
ManYu <manyu@hongtastock.com>
2024-07-15 16:36:54 +0800

    feat: 新增部署方式选择

--- modules_deploy/run.sh
+++ modules_deploy/run.sh
@@ -2,6 +2,11 @@
 echo "DolphinDB Module Deploy!"
 CURRENTIP=`hostname -I | awk '{ print $1 }'`
 echo "当前服务器地址: $CURRENTIP"
+echo "请选择部署方式:"
+echo "1. 本机部署"
+echo "2. 本机及远程部署"
+read -p "输入选择: " choice
+
 read -p "需要部署的文件名: " fileName
 
 read -p "是否执行？(Y/N): " confirm && [[ $confirm == [yY] ]] || exit 1
@@ -15,6 +20,21 @@ nodePathArr=(/dnode1/modules /dnode2/modules)
 remoteAddressArr=(192.168.56.105 192.168.56.106)
 remoteUsername=root
 remotePassword=root@1234
+
+if [ "$choice" == "1" ]; then
+# 本机文件复制
+echo "本机地址1: ${localPath}"
+echo "当前节点列表："
+echo "1. dnode1"
+echo "2. dnode2"
+read -p "请选择本机节点: " nodeIndex
+echo "本机地址2: ${prePath}${nodePathArr[$nodeIndex-1]}"
+`cp $originPath ${localPath} -f`
+`cp $originPath ${prePath}${nodePathArr[$nodeIndex-1]} -f`
+cd /home/dolphindb/server/
+./dolphindb -remoteHost 127.0.0.1 -remotePort 8902 -uid admin -pwd 123456 -run /home/moduleTest/moduleCacheClean.dos
+cd /home/moduleTest
+else
 for((i=0;i < ${#remoteAddressArr[*]};i++))
 do
 curRemoteAddress=${remoteAddressArr[$i]}
@@ -24,6 +44,9 @@ echo "本机地址1: ${localPath}"
 echo "本机地址2: ${prePath}${nodePathArr[$i]}"
 `cp $originPath ${localPath} -f`
 `cp $originPath ${prePath}${nodePathArr[$i]} -f`
+cd /home/dolphindb/server/
+./dolphindb -remoteHost 127.0.0.1 -remotePort 8902 -uid admin -pwd 123456 -run /home/moduleTest/moduleCacheClean.dos
+cd /home/moduleTest
 else
 # 远程环境文件复制
 # scp Module文件到指定服务器
@@ -35,7 +58,7 @@ spawn scp -r $originPath $remoteUsername@${remoteAddressArr[$i]}:${localPath}
 expect "password" { send "$remotePassword\n" }
 expect eof    
 # scp module缓存清理文件到指定服务器  
-spawn scp -r ./moduleCacheClean.dos $remoteUsername@${remoteAddressArr[$i]}:/home}
+spawn scp -r ./moduleCacheClean.dos $remoteUsername@${remoteAddressArr[$i]}:/home
 expect "password" { send "$remotePassword\n" } 
 expect eof  
 # 登录指定服务器，执行module缓存清理，以便在线更新module文件
@@ -48,3 +71,4 @@ expect eof
 EOF
 fi
 done
+fi
